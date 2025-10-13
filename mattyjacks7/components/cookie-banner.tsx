db58b01355a11de36914fcceca87b66ffeba4470
mattyjacks <98132678+mattyjacks@users.noreply.github.com>
2025-10-13 19:14:01 -0400

    Fix unused import

--- mattyjacks7/components/cookie-banner.tsx
+++ mattyjacks7/components/cookie-banner.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
-import { X } from "lucide-react";
 
 export default function CookieBanner() {
   const [isVisible, setIsVisible] = useState(false);
